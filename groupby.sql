483c2846a99fe094722e5afe918fea5284a74167
Rajeshwari <99334198+Rajeshwari-CuriousData@users.noreply.github.com>
2022-02-18 17:05:02 +0530

    Add files via upload

---
+++ groupby.sql
@@ -0,0 +1,8 @@
+use sakila;
+select * from language;
+select *  from language group by name;
+select *  from language group by name order by name;
+
+select * from payment;
+select * from payment group by customer_id order by payment_date;select *  from language group by name;
+
