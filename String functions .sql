6b9c70a3e71c8dbe71fc7ed2631a0910ed6f6763
Rajeshwari <99334198+Rajeshwari-CuriousData@users.noreply.github.com>
2022-02-18 16:58:27 +0530

    Add files via upload

---
+++ String functions .sql
@@ -0,0 +1,17 @@
+select "   Raji";
+select  ltrim ("   Raji");
+
+select "PYTHON";
+select lower("PYTHON");
+
+select "python";
+select upper("python");
+
+select "python";
+select reverse("python");
+
+select "hello adm";
+select substring("hello adm",2,7);
+
+select "hello adm";
+select substring("hello adm",1,7);
